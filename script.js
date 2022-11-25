ea8ab11c3bbe1c98f3193ea607d51dfcfdc9a08d
iskoclordu <abdulkadirlivaoglu@gmail.com>
2022-11-25 15:49:33 +0300

    Add Loop for 5 Round.Edit Result Message for Player to See Computer Choice.

--- script.js
+++ script.js
@@ -11,20 +11,20 @@ let computerSelection= getComputerChoice();
 
 let playerSelection=prompt("Choose your weapon!Rock, Paper or Scissors?");
 
-function singleRound(){
+function singleRound(playerSelection,computerSelection){
     if (playerSelection.toLowerCase()=='rock'&&computerSelection=='Rock'){
         let message='No Winner! It\'s a draw';
-        console.log(message);
+        console.log(`You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`);
     }
 
     else if (playerSelection.toLowerCase()=='rock'&&computerSelection=='Paper') {
         let message='You Lose! Paper beats Rock.';
-        console.log(message);
+        console.log(`You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`);
     }
 
     else if (playerSelection.toLowerCase()=='rock'&&computerSelection=='Scissors') {
         let message='You Win! Rock beats Scissors.';
-        console.log(message);
+        console.log(`You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`);
     }
 
 
@@ -32,17 +32,17 @@ function singleRound(){
 
     else if (playerSelection.toLowerCase()=='paper'&&computerSelection=='Rock') {
         let message='You Win! Paper beats Rock.';
-        console.log(message);
+        console.log(`You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`);
     }
 
     else if (playerSelection.toLowerCase()=='paper'&&computerSelection=='Paper') {
         let message='No Winner! It\'s a draw';
-        console.log(message);
+        console.log(`You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`);
     }
 
     else if (playerSelection.toLowerCase()=='paper'&&computerSelection=='Scissors') {
         let message='You Lose! Scissors beats Paper.';
-        console.log(message);
+        console.log(`You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`);
     }
 
 
@@ -50,17 +50,17 @@ function singleRound(){
 
     else if (playerSelection.toLowerCase()=='scissors'&&computerSelection=='Rock') {
         let message='You Lose! Rock beats Scissors.';
-        console.log(message);
+        console.log(`You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`);
     }
 
     else if (playerSelection.toLowerCase()=='scissors'&&computerSelection=='Paper') {
         let message='You Win! Scissors beats Paper.';
-        console.log(message);
+        console.log(`You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`);
     }
 
     else if (playerSelection.toLowerCase()=='scissors'&&computerSelection=='Scissors') {
         let message='No Winner! It\'s a draw';
-        console.log(message);
+        console.log(`You choose ${playerSelection}. Computer choose ${computerSelection}. \n${message}`);
     }
     
     else {
@@ -69,4 +69,11 @@ function singleRound(){
     }
 }
 
-singleRound();
+for (let i=1; i<6; i++){
+    if (i) {
+        let computerSelection= getComputerChoice();
+        let playerSelection=prompt("Choose your weapon!Rock, Paper or Scissors?");
+        singleRound(playerSelection,computerSelection);
+        console.log(`Round ${i}`);
+    }
+}
