60f138e97c1106c1b137dc30022c139a6a9b0dab
iskoclordu <abdulkadirlivaoglu@gmail.com>
2022-11-25 15:06:00 +0300

    Add Function for single Round

--- script.js
+++ script.js
@@ -8,3 +8,65 @@ function getComputerChoice(){
     return array[Math.floor(Math.random()*array.length)];
 }
 let computerSelection= getComputerChoice();
+
+let playerSelection=prompt("Choose your weapon!Rock, Paper or Scissors?");
+
+function singleRound(){
+    if (playerSelection.toLowerCase()=='rock'&&computerSelection=='Rock'){
+        let message='No Winner! It\'s a draw';
+        console.log(message);
+    }
+
+    else if (playerSelection.toLowerCase()=='rock'&&computerSelection=='Paper') {
+        let message='You Lose! Paper beats Rock.';
+        console.log(message);
+    }
+
+    else if (playerSelection.toLowerCase()=='rock'&&computerSelection=='Scissors') {
+        let message='You Win! Rock beats Scissors.';
+        console.log(message);
+    }
+
+
+
+
+    else if (playerSelection.toLowerCase()=='paper'&&computerSelection=='Rock') {
+        let message='You Win! Paper beats Rock.';
+        console.log(message);
+    }
+
+    else if (playerSelection.toLowerCase()=='paper'&&computerSelection=='Paper') {
+        let message='No Winner! It\'s a draw';
+        console.log(message);
+    }
+
+    else if (playerSelection.toLowerCase()=='paper'&&computerSelection=='Scissors') {
+        let message='You Lose! Scissors beats Paper.';
+        console.log(message);
+    }
+
+
+    
+
+    else if (playerSelection.toLowerCase()=='scissors'&&computerSelection=='Rock') {
+        let message='You Lose! Rock beats Scissors.';
+        console.log(message);
+    }
+
+    else if (playerSelection.toLowerCase()=='scissors'&&computerSelection=='Paper') {
+        let message='You Win! Scissors beats Paper.';
+        console.log(message);
+    }
+
+    else if (playerSelection.toLowerCase()=='scissors'&&computerSelection=='Scissors') {
+        let message='No Winner! It\'s a draw';
+        console.log(message);
+    }
+    
+    else {
+        let message='Please check your weapon choice!';
+        console.log(message);
+    }
+}
+
+singleRound();
