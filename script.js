a9f9fc18f71be487880fbef8f14a95ee02e47d73
iskoclordu <abdulkadirlivaoglu@gmail.com>
2022-11-25 16:09:24 +0300

    Add Function Game. Change singleRound Function from console.log to return.

--- script.js
+++ script.js
@@ -14,17 +14,17 @@ let playerSelection=prompt("Choose your weapon!Rock, Paper or Scissors?");
 function singleRound(playerSelection,computerSelection){
     if (playerSelection.toLowerCase()=='rock'&&computerSelection=='Rock'){
         let message='No Winner! It\'s a draw';
-        console.log(`You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`);
+        return `You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`;
     }
 
     else if (playerSelection.toLowerCase()=='rock'&&computerSelection=='Paper') {
         let message='You Lose! Paper beats Rock.';
-        console.log(`You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`);
+        return `You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`;
     }
 
     else if (playerSelection.toLowerCase()=='rock'&&computerSelection=='Scissors') {
         let message='You Win! Rock beats Scissors.';
-        console.log(`You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`);
+        return `You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`;
     }
 
 
@@ -32,17 +32,17 @@ function singleRound(playerSelection,computerSelection){
 
     else if (playerSelection.toLowerCase()=='paper'&&computerSelection=='Rock') {
         let message='You Win! Paper beats Rock.';
-        console.log(`You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`);
+        return `You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`;
     }
 
     else if (playerSelection.toLowerCase()=='paper'&&computerSelection=='Paper') {
         let message='No Winner! It\'s a draw';
-        console.log(`You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`);
+        return `You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`;
     }
 
     else if (playerSelection.toLowerCase()=='paper'&&computerSelection=='Scissors') {
         let message='You Lose! Scissors beats Paper.';
-        console.log(`You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`);
+        return `You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`;
     }
 
 
@@ -50,30 +50,35 @@ function singleRound(playerSelection,computerSelection){
 
     else if (playerSelection.toLowerCase()=='scissors'&&computerSelection=='Rock') {
         let message='You Lose! Rock beats Scissors.';
-        console.log(`You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`);
+        return `You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`;
     }
 
     else if (playerSelection.toLowerCase()=='scissors'&&computerSelection=='Paper') {
         let message='You Win! Scissors beats Paper.';
-        console.log(`You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`);
+        return `You choose ${playerSelection}. Computer choose ${computerSelection}.\n${message}`;
     }
 
     else if (playerSelection.toLowerCase()=='scissors'&&computerSelection=='Scissors') {
         let message='No Winner! It\'s a draw';
-        console.log(`You choose ${playerSelection}. Computer choose ${computerSelection}. \n${message}`);
+        return `You choose ${playerSelection}. Computer choose ${computerSelection}. \n${message}`;
     }
     
     else {
         let message='Please check your weapon choice!';
-        console.log(message);
+        return `${message}`;
     }
 }
 
-for (let i=1; i<6; i++){
-    if (i) {
+function game(){
+    for (let i=1; i<6; i++){
+        if (i) {
         let computerSelection= getComputerChoice();
         let playerSelection=prompt("Choose your weapon!Rock, Paper or Scissors?");
-        singleRound(playerSelection,computerSelection);
+        console.log(singleRound(playerSelection,computerSelection));
         console.log(`Round ${i}`);
+        }
     }
 }
+
+game();
+
