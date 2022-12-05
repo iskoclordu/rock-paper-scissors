0279d65660ee1356d7fa00bd74f4b1d576932d81
iskoclordu <abdulkadirlivaoglu@gmail.com>
2022-12-05 16:24:32 +0300

    Add Basic User Interface.

--- script.js
+++ script.js
@@ -4,43 +4,47 @@ let array=[
     "Scissors"
 ];
 
+let winner='' ;
+
 function getComputerChoice(){
     return array[Math.floor(Math.random()*array.length)];
 }
 
-function singleRound(){
-    let x= prompt("Choose your weapon!Rock, Paper or Scissors?");
+function singleRound(x){
     let y= getComputerChoice();
     if (x.toLowerCase()=='rock'&&y=='Rock'){
-        let message='No Winner! It\'s a tie';
-        return `You choose ${x}. Computer choose ${y}.\n${message}`;
+        winner = 'Draw';
+        return message=`You choose ${x}. Computer choose ${y}.`;
+        
     }
 
     else if (x.toLowerCase()=='rock'&&y=='Paper') {
-        let message='You Lose! Paper beats Rock.';
-        return `You choose ${x}. Computer choose ${y}.\n${message}`;
+        winner = 'Computer';
+        return message=`You choose ${x}. Computer choose ${y}.`;
+        
+        
     }
 
     else if (x.toLowerCase()=='rock'&&y=='Scissors') {
-        let message='You Win! Rock beats Scissors.';
-        return `You choose ${x}. Computer choose ${y}.\n${message}`;
+        winner = 'Human';
+        return message=`You choose ${x}. Computer choose ${y}.`;
     }
 
 
 
 
     else if (x.toLowerCase()=='paper'&&y=='Rock') {
-        let message='You Win! Paper beats Rock.';
+        winner = 'Human';
         return `You choose ${x}. Computer choose ${y}.\n${message}`;
     }
 
     else if (x.toLowerCase()=='paper'&&y=='Paper') {
-        let message='No Winner! It\'s a tie';
+        winner = 'Draw';
         return `You choose ${x}. Computer choose ${y}.\n${message}`;
     }
 
     else if (x.toLowerCase()=='paper'&&y=='Scissors') {
-        let message='You Lose! Scissors beats Paper.';
+        winner = 'Computer';
         return `You choose ${x}. Computer choose ${y}.\n${message}`;
     }
 
@@ -48,28 +52,27 @@ function singleRound(){
     
 
     else if (x.toLowerCase()=='scissors'&&y=='Rock') {
-        let message='You Lose! Rock beats Scissors.';
-        return `You choose ${x}. Computer choose ${y}.\n${message}`;
+        winner = 'Computer';
+        return `You choose ${x}. Computer choose ${y}.\n${message}` ;
     }
 
     else if (x.toLowerCase()=='scissors'&&y=='Paper') {
-        let message='You Win! Scissors beats Paper.';
+        winner = 'Human';
         return `You choose ${x}. Computer choose ${y}.\n${message}`;
     }
 
     else if (x.toLowerCase()=='scissors'&&y=='Scissors') {
-        let message='No Winner! It\'s a tie';
+        winner = 'Draw';
         return `You choose ${x}. Computer choose ${y}. \n${message}`;
     }
     
     else {
-        let message='Please check your weapon choice!';
-        return `${message}`;
+    
     }
 }
 
-function game(){
-    for (let i=1; i<6; i++){
+function game(n){
+    for (let i=1; i<=n; i++){
        
         console.log(singleRound());
         console.log(`Round ${i}`);
@@ -77,5 +80,133 @@ function game(){
     }
 }
 
-game();
+const resultContainer=document.querySelector('.result');
+
+const result=document.createElement('div');
+result.classList.add('result-round');
+resultContainer.appendChild(result);
+
+const win=document.createElement('p');
+win.classList.add('winner-text');
+resultContainer.insertBefore(win,result);
+win.textContent='Winner:'
+
+
+
+const score= document.createElement('div');
+score.classList.add('result-score');
+resultContainer.appendChild(score);
+let p=0
+let c=0
+score.textContent= `Human: ${p} - Computer: ${c}`;
+
+
+
+const rock= document.querySelector('#rock');
+rock.addEventListener('click',function(){
+    singleRound('rock');
+    win.textContent = 'Winner is: ' + winner;
+    result.textContent = message;
+
+        if (winner==='Human') {
+            p += 1
+        }
+
+        else if (winner==='Computer') {
+            c += 1
+        }
+
+        else {}
+  
+        score.textContent=`Human: ${p} - Computer: ${c}`;
+
+        if (p+c===5) {
+
+            if(p>c) {
+                score.textContent=`Human: ${p} - Computer: ${c}/ Winner is Human.` 
+
+            }
+
+            else {
+                score.textContent=`Human: ${p} - Computer: ${c}/ Winner is Computer.`
+            }
+
+            p=0
+            c=0
+
+
+        } 
+});
+
+const paper= document.querySelector('#paper');
+paper.addEventListener('click',function(){
+    singleRound('paper');
+    win.textContent = 'Winner is: ' + winner;
+    result.textContent = message;
+
+        if (winner==='Human') {
+            p += 1
+        }
+
+        else if (winner==='Computer') {
+            c += 1
+        }
+
+        else {}
+  
+        score.textContent=`Human: ${p} - Computer: ${c}`;
+
+        if (p+c===5) {
+
+            if(p>c) {
+                score.textContent=`Human: ${p} - Computer: ${c}/ Winner is Human.` 
+
+            }
+
+            else {
+                score.textContent=`Human: ${p} - Computer: ${c}/ Winner is Computer.`
+            }
+
+            p=0
+            c=0
+
+
+        } 
+});
+
+const scissors= document.querySelector('#scissors');
+scissors.addEventListener('click',function(){
+    singleRound('scissors');
+    win.textContent = 'Winner is: ' + winner;
+    result.textContent = message;
+
+        if (winner==='Human') {
+            p += 1
+        }
+
+        else if (winner==='Computer') {
+            c += 1
+        }
+
+        else {}
+  
+        score.textContent=`Human: ${p} - Computer: ${c}`;
+
+        if (p+c===5) {
+
+            if(p>c) {
+                score.textContent=`Human: ${p} - Computer: ${c}/ Winner is Human.` 
+
+            }
+
+            else {
+                score.textContent=`Human: ${p} - Computer: ${c}/ Winner is Computer.`
+            }
+
+            p=0
+            c=0
+
+
+        } 
+});
 
